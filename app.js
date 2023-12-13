de2127f1fe57f1b3c9030e08d1feb03ed61a00dd
nxmoulleb <nxmoulleb@gmail.com>
2023-12-13 05:08:01 -0500

    frontend_url environment var was not being used in app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "origin: process.env.FRONTEND_URL",
+    origin: process.env.FRONTEND_URL,
   })
 );
 const sessionOptions = {
