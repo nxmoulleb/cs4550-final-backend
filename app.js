ef698d2f787c69c64bf1b8249a02ee00b87cb4da
nxmoulleb <nxmoulleb@gmail.com>
2023-12-13 04:46:44 -0500

    .env in gitignore

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ import mongoose from "mongoose";
 import UserRoutes from "./users/userRoutes.js";
 import CollectionRoutes from "./collection/collectionRoutes.js";
 import ReviewRoutes from "./reviews/reviewRoutes.js";
-mongoose.connect("mongodb://127.0.0.1:27017/final");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/final'
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "origin: process.env.FRONTEND_URL",
   })
 );
 const sessionOptions = {
@@ -19,6 +20,13 @@ const sessionOptions = {
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
